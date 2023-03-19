6f737da60add01611c0a678ef63821f9c1d7f00d
CoderWanFeng <875143930@qq.com>
2023-03-19 20:52:21 +0800

    publish 0.0.3 : add merge4docx👉https://www.bilibili.com/video/BV1Vo4y1q7i3

--- poword/api/word.py
+++ poword/api/word.py
@@ -3,7 +3,7 @@
 
 #############################################
 # File Name: word.py
-# Author: 程序员晚枫
+# 公众号/B站/小红书/抖音: 程序员晚枫
 # Mail: 1957875073@qq.com
 # Created Time:  2022-4-25 10:17:34
 # Description: 有关word的自动化操作
@@ -20,4 +20,6 @@ mainWord = MainWord()
 # 为了适配wps不能转换doc的问题，这里限定：只能转换docx
 # @except_dec()
 def docx2pdf(path):
-    mainWord.file2pdf(path)
+    mainWord.docx2pdf(path)
+def merge4docx(input_path, output_path, new_word_name):
+    mainWord.merge4docx(input_path, output_path, new_word_name)
--- poword/core/WordType.py
+++ poword/core/WordType.py
@@ -1,10 +1,11 @@
 import os
 from win32com.client import constants, gencache
-
+# import win32com.client as win32
+from pathlib import Path
 
 class MainWord():
 
-    def file2pdf(self, path, docxSuffix=".docx"):
+    def docx2pdf(self, path, docxSuffix=".docx"):
         wordFiles = []
         # 如果不存在，则不做处理
         if not os.path.exists(path):
@@ -31,10 +32,25 @@ class MainWord():
             print(pdfPath)
             self.createpdf(filepath, pdfPath)
 
-
-    def createpdf(self,wordPath, pdfPath):
+    def createpdf(self, wordPath, pdfPath):
         word = gencache.EnsureDispatch('Word.Application')
         doc = word.Documents.Open(wordPath, ReadOnly=1)
         # 转换方法
         doc.ExportAsFixedFormat(pdfPath, constants.wdExportFormatPDF)
         word.Quit()
+
+    def merge4docx(self, input_path, output_path, new_word_name):
+        abs_input_path = Path(input_path).absolute()  # 相对路径→绝对路径
+        abs_output_path = Path(output_path).absolute()  # 相对路径→绝对路径
+        save_path = abs_output_path / new_word_name
+        print('-' * 10 + '开始合并!' + '-' * 10)
+        word_app = gencache.EnsureDispatch('Word.Application')   # 打开word程序
+        word_app.Visible = False  # 是否可视化
+        folder = Path(abs_input_path)
+        waiting_files = [path for path in folder.iterdir()]
+        output_file = word_app.Documents.Add()  # 新建合并后的文档
+        for single_file in waiting_files:
+            output_file.Application.Selection.InsertFile(single_file)  # 拼接文档
+        output_file.SaveAs(str(save_path))  # 保存
+        output_file.Close()
+        print('-' * 10 + '合并完成!' + '-' * 10)
